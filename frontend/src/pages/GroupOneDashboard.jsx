b33cc2973bcd771f874b964b06a079b38b144cc0
StormVRguy <spirit.info300@gmail.com>
2026-05-30 06:37:51 +0200

    new instructions

--- frontend/src/pages/GroupOneDashboard.jsx
+++ frontend/src/pages/GroupOneDashboard.jsx
@@ -8,21 +8,10 @@ function GroupOneIntro() {
       <h2>PRATICHE QUOTIDIANE</h2>
       <ul className="daily-practices-list">
         <li>
-          <strong>PRATICA FORMALE</strong>: Meditare quotidianamente in posizione seduta seguendo l&apos;ultima traccia audio.
+          <strong>PRATICA FORMALE</strong>: Praticare ogni giorno seguendo la traccia audio.
         </li>
         <li>
-          <strong>TASK CONTEMPLATIVO</strong>: Qualche volta provare ad addormentarsi osservando la mente, proprio come facciamo in meditazione, ma senza contrastare il processo di addormentamento. Meglio se questa esplorazione è svolta in una delle seguenti circostanze: dopo un risveglio notturno; al mattino; oppure di pomeriggio. Ciò ha due scopi:
-          <ul>
-            <li>
-              È di per sé interessante osservare la mente durante lo stato in cui i sensi sono quasi sopiti e le forme consuete di attività mentale cedono il passo a processi interiori più vividi e bizzarri – benché non facili da contemplare senza perdere coscienza.
-            </li>
-            <li>
-              Questa attività potrebbe condurre, senza ulteriori pratiche, a sperimentare sogni lucidi (o comunque sogni più vividi e consapevoli del consueto), associati a questo tipo di meditazioni in varie fonti contemplative.
-            </li>
-          </ul>
-        </li>
-        <li>
-          <strong>PRATICA INFORMALE</strong>: Provare ad accorgersi se, nella vita di tutti i giorni, si riesce a essere più consapevoli dei propri processi interiori, quasi come se questi venissero percepiti attraverso un senso interno. Riusciamo a sperimentare una relativa continuità di consapevolezza? Abbiamo l&apos;impressione che in tal modo si riduca il senso di identificazione con i contenuti e processi mentali più intensi e/o se ne limiti la durata?
+          <strong>PRATICA INFORMALE</strong>: Possiamo provare a mantenere una relativa continuità di presenza durante le attività quotidiane, rafforzando la particolare forma di consapevolezza che stiamo scoprendo e coltivando nella pratica formale - la quale potrebbe essere descritta come lo &quot;spazio&quot; entro il quale sorgono e svaniscono i vari fenomeni dell&apos;esperienza, e che in ultima analisi non è neppure distinguibile da questi ultimi.
         </li>
       </ul>
     </div>
